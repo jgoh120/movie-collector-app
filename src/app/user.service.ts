f3a2137bd7f3693f2a3923d2c5b097a783cc7ef0
Clarence Castillo <clarencecastillo@outlook.com>
2020-07-28 18:49:08 +0800

    Refactor auth service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,16 +10,25 @@ const apiUrl2 = 'http://localhost:3000/user';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
+  
+  //public movieList:User[] = []
+  constructor(private http: HttpClient) { }
+  
+  getAll() {
+    return this.http.get<User[]>(`${apiUrl}/users`);
+  }
+  
+  addUser(user: User): Observable<any>{
+    return this.http.post(apiUrl2, user,  {responseType: 'text'}); 
+  }
+  
+}
 
-    //public movieList:User[] = []
-    constructor(private http: HttpClient) { }
-
-    getAll() {
-        return this.http.get<User[]>(`${apiUrl}/users`);
-    }
-
-    addUser(user: User): Observable<any>{
-        return this.http.post(apiUrl2, user,  {responseType: 'text'}); 
-      }
-    
+export type User = {
+  id: string;
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  contact: string;
 }
