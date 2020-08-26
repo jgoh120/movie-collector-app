f27597a73a53b6ae0ceb1f3db9a9b227bfc49da4
Justin Goh <justin1_goh@yahoo.com.sg>
2020-08-26 19:25:21 +0800

    Make reviews reactive

--- src/app/review.service.ts
+++ src/app/review.service.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { environment } from '../environments/environment';
+import { mergeMap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 
 export class ReviewService {
   
+  private reviewChange: BehaviorSubject<void>;
+
   constructor(
     private http: HttpClient
-  ) { }
+  ) { 
+    this.reviewChange = new BehaviorSubject(null);
+  }
 
   async create(movieId: string, review: NewReview): Promise<void> {
-    await this.http.post(`${environment.apiUrl}/movies/${movieId}/reviews`, review).toPromise();
+    await this.http.post(`${environment.apiUrl}/movies/${movieId}/reviews`, review, { responseType: 'text' }).toPromise();
+    this.reviewChange.next();
   }
 
   getByMovieId(movieId: string): Observable<Review[]> {
-    return this.http.get<Review[]>(`${environment.apiUrl}/movies/${movieId}/reviews`);
+    return this.reviewChange.asObservable().pipe(
+      mergeMap(() => this.http.get<Review[]>(`${environment.apiUrl}/movies/${movieId}/reviews`))
+    );    
   }
-
-
 }
 
 export type NewReview = {
